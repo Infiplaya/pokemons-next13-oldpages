08571d3b06bc85b51f92d4c2f35e194b0b62294f
Pawel Sobaniec <infipablosoban@gmail.com>
2022-11-09 14:18:14 +0100

    first magic

---
+++ pages/pokemon/[id].js
@@ -0,0 +1,77 @@
+import { useRouter } from "next/router";
+import { observer } from "mobx-react";
+import {
+  CssBaseline,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@material-ui/core";
+import styled from "@emotion/styled";
+
+
+const PageContainer = styled.div`
+  margin: auto;
+  width: 800px;
+  padding-top: 1em;
+`;
+const TypeHeader = styled.span`
+  font-weight: bold;
+`;
+
+export async function getStaticPaths() {
+  const allPokemon = require('../../src/pokemon.json');
+  return {
+    paths: allPokemon.map((p) => ({
+      params: {
+        id: p.id.toString(),
+      }
+    })),
+    fallback: false // See the "fallback" section below
+  };
+}
+
+export const getStaticProps = async (context) => {
+  const allPokemon = require('../../src/pokemon.json')
+  const pokemon = allPokemon.find((p) => p.id === parseInt(context.params.id));
+  return {
+    props: { pokemon },
+  };
+};
+
+export default observer(({ pokemon }) => {
+
+  return (
+    <PageContainer>
+      <CssBaseline />
+
+      <div>
+        {pokemon && (
+          <>
+            <h1>{pokemon.name.english}</h1>
+            <p>
+              <TypeHeader>Type:</TypeHeader> {" " + pokemon.type.join(", ")}
+            </p>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Attribute</TableCell>
+                  <TableCell>Value</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {Object.keys(pokemon.base).map((key) => (
+                  <TableRow key={key}>
+                    <TableCell>{key}</TableCell>
+                    <TableCell>{pokemon.base[key]}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </>
+        )}
+      </div>
+    </PageContainer>
+  );
+});
---
+++ src/store.js
@@ -0,0 +1,25 @@
+import { makeAutoObservable } from "mobx";
+
+class Store {
+  pokemon = require('./pokemon.json');
+  filter = "";
+  selectedPokemon = null;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  setPokemon(pokemon) {
+    this.pokemon = pokemon;
+  }
+  setFilter(filter) {
+    this.filter = filter;
+  }
+  setSelectedPokemon(selectedPokemon) {
+    this.selectedPokemon = selectedPokemon;
+  }
+}
+
+const store = new Store();
+
+export default store;
